950419c5652f853b29f2dbabad98fa05fa0768c8
Ladson Leno <ladsonlmnunes@recife.pe.senac.br>
2021-02-24 09:44:14 -0300

    mensagem

---
+++ public/sw.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'staticV4';
+
+this.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll([
+                './',
+
+                //
+                '../css/w3.css',
+                '../css/materialdesignicons.css',
+                '../css/materialdesignicons.css.map',
+                 //
+                 '../assets/favicon.ico',
+                '../assets/img.jpg',
+                '../assets/logo.png',
+
+
+                //
+                '../fonts/materialdesignicons-webfont.woff2?v=5.8.55',
+                //
+                '../manifest.json',
+             ]);
+        })
+    );
+});
+
+self.addEventListener('activate', function activator(event) {
+    event.waitUntil(
+        caches.keys().then(function(keys) {
+            return Promise.all(keys
+                .filter(function(key) {
+                    return key.indexOf(CACHE_NAME) !== 0;
+                })
+                .map(function(key) {
+                    return caches.delete(key);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(cachedResponse) {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
